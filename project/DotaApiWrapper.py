9a5e3bef810246c02135d8c5ae20f3ed64dcad4a
Robert <rrochlin@yahoo.com>
2023-08-27 11:01:30 -0700

    initial commit

---
+++ project/DotaApiWrapper.py
@@ -0,0 +1,28 @@
+from requests import get
+from time import sleep
+
+API_BASE = r"https://api.opendota.com/api/{}?api_key={}"
+
+def fetch_response(request):
+    for _ in range(3):
+        response = get(request)
+        if (response.status_code == 200):
+            return response
+        sleep(1)
+    raise Exception("api call failed")
+
+class DotaAPIWrapper:
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+        
+    def get_player(self, player_id):
+        return fetch_response(API_BASE.format(f"players/{player_id}", self.api_key))
+    
+    def get_heroes(self):
+        return fetch_response(API_BASE.format(f"heroes", self.api_key))
+    
+    def get_hero_stats(self):
+        return fetch_response(API_BASE.format(f"heroStats", self.api_key))
+    
+    def get_constants(self):
+        return fetch_response(API_BASE.format(f"constants", self.api_key))
---
+++ project/GetDotaHeroImages.py
@@ -0,0 +1,30 @@
+from requests import get
+from configparser import ConfigParser
+from DotaApiWrapper import DotaAPIWrapper
+from shutil import copyfileobj
+import multiprocessing as mp
+from pathlib import Path
+from itertools import repeat
+
+
+def get_hero_info(home: Path) -> list:
+    config = ConfigParser()
+    config.read(Path(home, "Web.config"))
+    config["SECRETS"]["api_key"]
+    api = DotaAPIWrapper(config["SECRETS"]["api_key"])
+    return api.get_hero_stats().json()
+
+def download_image(info: dict, home: Path) -> None:
+    r = get(f"https://cdn.dota2.com/{info['img']}", stream=True)
+    file = Path(home, f"project/assetts/images/{info['localized_name']}.png")
+    with open(file, "wb") as out_file:
+        copyfileobj(r.raw, out_file)
+
+def main():
+    home = Path(__file__).parent.parent.resolve()
+    hero_info = get_hero_info(home)
+    with mp.Pool(mp.cpu_count()//2) as pool:
+        pool.starmap(download_image, zip(hero_info, repeat(home)))
+
+if __name__ == "__main__":
+    main()
