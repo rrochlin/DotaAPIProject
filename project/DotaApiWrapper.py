427e4dc30e06aacd374f35c5ec447ec613859b91
Robert Rochlin <robertrochlin@MacBook-Air.local>
2023-09-16 13:17:53 -0700

    updated gitignore file structure

--- project/DotaApiWrapper.py
+++ project/DotaApiWrapper.py
@@ -1,5 +1,8 @@
-from requests import get
+from requests import get, Response
 from time import sleep
+from pickle import dump, load
+from os import getcwd
+from pathlib import Path
 
 API_BASE = r"https://api.opendota.com/api/{}?api_key={}"
 
@@ -14,21 +17,28 @@ def fetch_response(request):
 class DotaAPIWrapper:
     def __init__(self, api_key: str):
         self.api_key = api_key
+        self.heroes = self.get_heroes()
         
-    def get_player(self, player_id):
+    def get_player(self, player_id) -> Response:
         return fetch_response(API_BASE.format(f"players/{player_id}", self.api_key))
     
-    def get_heroes(self):
-        return fetch_response(API_BASE.format(f"heroes", self.api_key))
+    def get_heroes(self, refresh=False) -> dict():
+        file_path = Path(getcwd(),"assetts","static_objects","heroes.pkl").resolve()
+        if refresh or not Path.exists(file_path):
+            heroes = fetch_response(API_BASE.format(f"heroes", self.api_key))
+            with open(file_path, "wb") as file:
+                dump({hero["id"]:hero for hero in heroes.json()}, file=file)
+        with open(file_path, "rb") as file:
+            return load(file=file)
     
-    def get_hero_stats(self):
+    def get_hero_stats(self) -> Response:
         return fetch_response(API_BASE.format(f"heroStats", self.api_key))
     
-    def get_constants(self):
+    def get_constants(self) -> Response:
         return fetch_response(API_BASE.format(f"constants", self.api_key))
 
-    def get_hero_matchups(self, hero_id:int):
+    def get_hero_matchups(self, hero_id:int) -> Response:
         return fetch_response(API_BASE.format(f"heroes/{hero_id}/matchups", self.api_key))
 
-    def get_hero_items(self, hero_id:int):
+    def get_hero_items(self, hero_id:int) -> Response:
         return fetch_response(API_BASE.format(f"heroes/{hero_id}/itemPopularity", self.api_key))
